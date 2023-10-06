16d99556725b437aee48cb2e421b475a0aaefbad
DmitryZakharo152 <zahadima@gmail.com>
2023-10-06 09:17:46 +0300

    Финал (до замечаний)

--- scripts/script.js
+++ scripts/script.js
@@ -4,77 +4,94 @@ const bulletsList = document.querySelector('.pagination');
 const bullits = document.querySelectorAll('.pagination-btn');
 const screens = document.querySelectorAll('.slider-item');
 
+
+const fedbackLink = document.querySelector('.fedback-btn');
+const modal = document.querySelector('.modal-overlay');
+const closeButton = document.querySelector('.modal-close-btn');
+
+
 if (screens) {
-    const bulletsArray = Array.from(bullits);
-    const screensArray = Array.from(screens);
+  const bulletsArray = Array.from(bullits);
+  const screensArray = Array.from(screens);
 
-    const model = [true, false, false];
-
-
-    const getCurrentElement = () => model.findIndex((index) => index);
-
-    const updateModel = (index) => {
-        model.forEach((item, i) => {
-            model[i] = i === index;
-        });
-    }
-
-    const renderActiveBullit = () => {
-        const currentIndex = getCurrentElement();
-        document.querySelector('.current-btn').classList.remove('current-btn');
-        bulletsArray[currentIndex].classList.add('current-btn');
-    };
+  const model = [true, false, false];
 
 
-    const renderActiveScreen = () => {
-        const currentIndex = getCurrentElement();
-        document.querySelector('.slide-active').classList.remove('slide-active');
-        screensArray[currentIndex].classList.add('slide-active');
+  const getCurrentElement = () => model.findIndex((index) => index);
 
-        screensArray.slice(currentIndex).forEach((item, i) => {
-            item.style.order = i
-        });
+  const updateModel = (index) => {
+    model.forEach((item, i) => {
+      model[i] = i === index;
+    });
+  };
 
-        screensArray.slice(0, currentIndex).forEach((item, i) => {
-            item.style.order = screensArray.length - currentIndex + i
-        });
+  const renderActiveBullit = () => {
+    const currentIndex = getCurrentElement();
+    document.querySelector('.current-btn').classList.remove('current-btn');
+    bulletsArray[currentIndex].classList.add('current-btn');
+  };
 
-        document.body.classList = [];
-        document.body.classList.add(`bg-${currentIndex + 1}`)
-    }
+  const renderActiveScreen = () => {
+    const currentIndex = getCurrentElement();
+    document.querySelector('.slide-active').classList.remove('slide-active');
+    screensArray[currentIndex].classList.add('slide-active');
 
-    const getPrevIndex = () => {
-        let current = getCurrentElement();
-        return current - 1 >= 0 ? current - 1 : model.length - 1;
-    }
-
-    const getNextIndex = () => {
-        let current = getCurrentElement();
-        return current + 1 <= model.length - 1 ? current + 1 : 0;
-    }
-
-
-    prevButtom.addEventListener('click', () => {
-        let index = getPrevIndex();
-        updateModel(index);
-        renderActiveScreen();
-        renderActiveBullit();
+    screensArray.slice(currentIndex).forEach((item, i) => {
+      item.style.order = i;
     });
 
-    nextButton.addEventListener('click', () => {
-        let index = getNextIndex();
-        updateModel(index);
-        renderActiveScreen();
-        renderActiveBullit();
+    screensArray.slice(0, currentIndex).forEach((item, i) => {
+      item.style.order = screensArray.length - currentIndex + i;
     });
 
-    bulletsList.addEventListener('click', (e) => {
-        if (e.target.classList.contains('pagination-btn')) {
-            let clickedButtonIndex = bulletsArray.indexOf(e.target);
-            updateModel(clickedButtonIndex);
-            renderActiveScreen();
-            renderActiveBullit();
-        }
-    });
+    document.body.classList = [];
+    document.body.classList.add(`bg-${currentIndex + 1}`);
+  };
+
+  const getPrevIndex = () => {
+    let current = getCurrentElement();
+    return current - 1 >= 0 ? current - 1 : model.length - 1;
+  };
+
+  const getNextIndex = () => {
+    let current = getCurrentElement();
+    return current + 1 <= model.length - 1 ? current + 1 : 0;
+  };
+
+
+  prevButtom.addEventListener('click', () => {
+    let index = getPrevIndex();
+    updateModel(index);
+    renderActiveScreen();
+    renderActiveBullit();
+  });
+
+  nextButton.addEventListener('click', () => {
+    let index = getNextIndex();
+    updateModel(index);
+    renderActiveScreen();
+    renderActiveBullit();
+  });
+
+  bulletsList.addEventListener('click', (e) => {
+    if (e.target.classList.contains('pagination-btn')) {
+      let clickedButtonIndex = bulletsArray.indexOf(e.target);
+      updateModel(clickedButtonIndex);
+      renderActiveScreen();
+      renderActiveBullit();
+    }
+  });
 
 }
+
+
+// для модалки
+fedbackLink.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  modal.classList.add('modal-show');
+});
+
+closeButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  modal.classList.remove('modal-show');
+});
